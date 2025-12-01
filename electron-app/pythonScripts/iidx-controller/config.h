77058a88e20427f19aa81dfe0489c25e16d9e744
kit <damig@onet.pl>
2025-12-01 20:01:46 +0100

    added IIDX-controller arduino scripts

---
+++ electron-app/pythonScripts/iidx-controller/config.h
@@ -0,0 +1,23 @@
+#ifndef _CONFIGURATION_H_
+#define _CONFIGURATION_H_
+#include <stdint.h>
+const uint8_t button_pins[] = {13,12,11,10,9,8,7,6,5};
+#define NUM_BUTTONS (sizeof(button_pins)/sizeof(button_pins[0]))
+#define ENCODER_CLK_PIN 14
+#define ENCODER_DT_PIN  15
+
+typedef struct {
+  uint16_t debounce_time;
+} configuration_struct;
+
+static configuration_struct global_config;
+
+inline void initialise_configuration() {
+  global_config.debounce_time = 10;
+}
+
+inline void get_configuration(configuration_struct **c) {
+  *c = &global_config;
+}
+
+#endif
---
+++ electron-app/pythonScripts/iidx-controller/encoder.h
@@ -0,0 +1,9 @@
+#ifndef _ENCODER_H_
+#define _ENCODER_H_
+#include <stdint.h>
+
+void initialise_encoder();
+int32_t get_encoder_state();
+int get_digital_encoder_state();
+
+#endif

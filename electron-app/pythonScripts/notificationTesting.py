ab5c5f9339c1503d3a326bc13efa2c8868f3f002
Damian G <damig@onet.pl>
2025-12-02 09:38:37 +0100

    das fun

--- electron-app/pythonScripts/notificationTesting.py
+++ electron-app/pythonScripts/notificationTesting.py
@@ -1,9 +1,2 @@
-import http.client, urllib
-conn = http.client.HTTPSConnection("api.pushover.net:443")
-conn.request("POST", "/1/messages.json",
-  urllib.parse.urlencode({
-    "token": "abc123",
-    "user": "user123",
-    "message": "hello world",
-  }), { "Content-type": "application/x-www-form-urlencoded" })
-conn.getresponse()
+from sys import platform
+print(platform)
